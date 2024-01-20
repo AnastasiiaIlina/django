a6b6aeb9e9ba8c68212ffa927a047624ec5e2505
Anastasiia <ilina@involve.software>
2024-01-20 21:43:47 +0200

    added hw31

--- notes_app/notes/apps.py
+++ notes_app/notes/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TodoProjectConfig(AppConfig):
+class NotesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'todo_project'
+    name = 'notes'
